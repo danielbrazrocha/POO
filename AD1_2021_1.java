5872988be2a9b81fcd4a62fb582639778a25aa96
Daniel Rocha <daniel.rocha@injunior.com.br>
2021-03-25 16:28:43 -0300

    add unverified Amigo class

--- AD1_2021_1.java
+++ AD1_2021_1.java
@@ -1,23 +1,28 @@
 package com.company;
 
-import java.util.Arrays;
-
 class Post {
     //Atributos da classe Post
     private String textoPost;
     private Amigo quemCurtiu[];
     private int qtdCurtidas;
+    private static short MAX_CURTIDAS = 100;
 
     //Construtor da classe Post
     public Post(String textoPost) {
         this.textoPost = textoPost;
         this.qtdCurtidas = 0;
+        this.quemCurtiu = new Amigo[MAX_CURTIDAS];
     }
 
     //Funcao para receber curtida, atualizando o contador e o array dos amigos que curtiram o post
     public void recebeCurtida(Amigo a) {
         this.quemCurtiu[qtdCurtidas] = a;
         this.qtdCurtidas++;
+    }
+
+    //Função para retornar a quantidade de curtidas do Post
+    public int getLikes() {
+        return this.qtdCurtidas;
     }
 
     public String retornaNomesQueCurtiram() {
@@ -40,25 +45,44 @@ class Post {
 class Amigo {
     //Atributos da classe Amigo
     private String nome;
+    private Post meusPosts[];
+    private int qtdPosts;
+    private static short MAX_POSTS = 100;
 
     //Construtor da classe Amigo
     public Amigo(String nome) {
         this.nome = nome;
+        this.qtdPosts = 0;
+        this.meusPosts = new Post[MAX_POSTS];
+    }
+
+    public void postar(Post p) {
+        this.meusPosts[qtdPosts] = p;
+        this.qtdPosts++;
+        System.out.println(this.nome + "postou o post" + p );
+
+    }
+
+    public void curtir(Post p) {
+        p.recebeCurtida(this);
+        System.out.println(this.nome + "curtiu o post" + p );
+    }
+
+    public Post retornaPostMaisCurtido() {
+        Post retorno = null;
+        int maior = 0;
+        for (Post p: meusPosts)
+            if (p.getLikes() > maior)
+                retorno = p;
+        return retorno;
+        //Verificar o retorno se está correto
     }
 
     public String toString() {
-        return this.nome;
+        return "Meu nome é" + this.nome + ", e tenho " + this.qtdPosts;
     }
 
-    public String postar(Post p) {
-        //p.
-        return null;
-    }
 
-    public String curtir(Post p) {
-        //p.
-        return null;
-    }
 }
 
 class Rede {
