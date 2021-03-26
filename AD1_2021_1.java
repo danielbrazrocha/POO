2a9dd9e3a340b66f7147d950f35fe18d8b7f5dec
Daniel Rocha <daniel.rocha@injunior.com.br>
2021-03-25 22:43:33 -0300

    fix major bugs

--- AD1_2021_1.java
+++ AD1_2021_1.java
@@ -1,6 +1,12 @@
 package com.company;
 
-import java.util.Arrays;
+/**
+ * CEDERJ
+ * Curso Superior de Tecnologia em Sistemas de Computação
+ * Disciplina: Programação Orientada a Objetos
+ * AD1 2021.1
+ * Aluno: Daniel Gustavo Braz Rocha   Matricula:20113050368    Polo:Pirai
+ */
 
 class Post {
     //Atributos da classe Post
@@ -16,31 +22,44 @@ class Post {
         this.quemCurtiu = new Amigo[MAX_CURTIDAS];
     }
 
-    //Funcao para receber curtida, atualizando o contador e o array dos amigos que curtiram o post
+    /**
+     * Métodos da classe Post
+     */
+
+    //Método para receber curtida, atualizando o contador e o array dos amigos que curtiram o post
     public void recebeCurtida(Amigo a) {
         this.quemCurtiu[qtdCurtidas] = a;
         this.qtdCurtidas++;
     }
 
-    //Função para retornar a quantidade de curtidas do Post
+    //Método para retornar a quantidade de curtidas do Post
     public int getLikes() {
         return this.qtdCurtidas;
     }
 
+    //Método para retornar uma String com o nome dos que curtiram o Post, separados por espaço
     public String retornaNomesQueCurtiram() {
-        String retorno = null;
-        for (Amigo a: quemCurtiu)
-            if (a != null)
-                retorno = retorno + a;
-            else
+        String retorno = "";
+        for (Amigo a: quemCurtiu) {
+            if (a != null) {
+                retorno = retorno + a.getAmigoNome() + " ";
+            }
+            else {
                 break;
+            }
+        }
         return retorno + "\n";
         //Verificar impressão se com o nome apenas ou se todos os dados de amigo
     }
 
-    // Retorno da classe
-    public String toString() {
+    // Método que retorna um resumo com os posts (para concatenação)
+    public String getPostsInline() {
         return this.textoPost + ": " + this.qtdCurtidas + " curtidas";
+    }
+
+    // Método que retorna a String de representação da classe Post
+    public String toString() {
+        return this.textoPost + ": " + this.qtdCurtidas + " curtidas" + "\n";
     }
 
 }
@@ -58,34 +77,61 @@ class Amigo {
         this.meusPosts = new Post[MAX_POSTS];
     }
 
+    /**
+     * Métodos da classe Amigo
+     */
+
+    //Método para um Amigo postar um Post
     public void postar(Post p) {
         this.meusPosts[qtdPosts] = p;
         this.qtdPosts++;
-        System.out.println(this.nome + "postou o post" + p );
+        //System.out.println(this.nome + "postou o post" + p );
 
     }
 
+    //Método para um Amigo curtir um Post
     public void curtir(Post p) {
         p.recebeCurtida(this);
-        System.out.println(this.nome + "curtiu o post" + p );
+        //System.out.println(this.nome + "curtiu o post" + p );
     }
 
+    //Método para retornar o Post que tenha o maior número de curtidas
     public Post retornaPostMaisCurtido() {
         Post retorno = null;
         int maior = 0;
-        for (Post p: meusPosts)
-            if (p.getLikes() > maior)
-                retorno = p;
+        for (Post p: meusPosts) {
+            if (p != null) {
+                if (p.getLikes() > maior)
+                    retorno = p;
+            } else {
+                break;
+            }
+        }
         return retorno;
-        //Verificar o retorno se está correto
     }
 
+    //Método para retornar o nome do Amigo
+    public String getAmigoNome() {
+        return this.nome;
+    }
+
+    // Método que retorna a String de representação da classe Amigo (usado para a timeline)
     public String toString() {
-        return this.nome + " :" + "\n" + Arrays.toString(this.meusPosts);
-        //Verificar se não é melhor um for
+        if (this.qtdPosts != 0) {
+            String retorno = this.nome + " :" + "\n";
+            for (Post p: meusPosts)
+                if (p != null){
+                    retorno = retorno + p.getPostsInline() + "\n";
+                }
+                else {
+                    break;
+                }
+            return retorno;
+        }
+        else {
+            return this.nome + " :" + "\n";
+        }
     }
-
-
 }
 
 class Rede {
@@ -100,19 +146,32 @@ class Rede {
         this.qtdAmigos = 0;
     }
 
-    //Função para adicionara Amigo a Rede
+    /**
+     * Métodos da classe Rede
+     */
+
+    //Método para adicionar Amigo a Rede
     public void adicionarAmigo(Amigo a) {
         this.amigosNaRede[qtdAmigos] = a;
         this.qtdAmigos++;
-        System.out.println(a + "foi adicionado a rede");
+        //System.out.println(a.getAmigoNome() + " foi adicionado a rede");
     }
 
-    public void timeline() {
-        for (Amigo a: amigosNaRede)
-            System.out.println(a.toString());
-        //Verificar o retorno se está correto
-
+    //Método que imprime a timeline da classe
+    public String timeline() {
+        String retorno = "";
+        for (Amigo a: amigosNaRede) {
+            if (a != null){
+                retorno = retorno + a;
+            }
+            else {
+                break;
+            }
+        }
+        return retorno;
     }
+
+    // Método que retorna a String de representação da classe Post
 }
 
 
@@ -120,6 +179,37 @@ class testePrint {
 
     public static void main(String[] args) {
         Amigo fulano = new Amigo("Fulano");
-        System.out.println(fulano);
+        Amigo ciclano = new Amigo("Ciclano");
+        Amigo beltrano = new Amigo("Beltrano");
+        Amigo outrano = new Amigo("Outrano");
+        Rede paraiso = new Rede();
+        paraiso.adicionarAmigo(fulano);
+        paraiso.adicionarAmigo(ciclano);
+        paraiso.adicionarAmigo(beltrano);
+        Post bomdia = new Post("Bom dia!");
+        Post boatarde = new Post("Boa tarde!");
+        Post boanoite = new Post("Boa noite!");
+        Post helloworld = new Post("Hello World!");
+        Post olatodos = new Post("Olá Todos!");
+        fulano.postar(bomdia);
+        fulano.postar(boatarde);
+        ciclano.postar(boanoite);
+        ciclano.postar(olatodos);
+        ciclano.postar(helloworld);
+        beltrano.curtir(helloworld);
+        fulano.curtir(helloworld);
+        outrano.curtir(helloworld);
+        beltrano.curtir(bomdia);
+        beltrano.curtir(boatarde);
+        beltrano.curtir(boanoite);
+        fulano.curtir(boanoite);
+        System.out.println("Timeline:");
+        System.out.println(paraiso.timeline());
+        System.out.println("Post mais curtido de um usuário:");
+        Post maiscurtido = ciclano.retornaPostMaisCurtido();
+        System.out.println(maiscurtido);
+        System.out.println("Quem curtiu: " +
+                maiscurtido.retornaNomesQueCurtiram());
+
     }
 }
